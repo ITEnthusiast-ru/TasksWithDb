18d82c0d12899b48d79076690e9c95bc261588e7
Igor Bazanov <itenthusiast.ru@gmail.com>
2025-08-13 19:31:18 +0700

    Добавьте файлы проекта.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TasksWithDb
+{
+    public class ApplicationDbContext:DbContext
+    {
+        DbSet<User> Users { get; set; }
+        public ApplicationDbContext() =>Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = helloApp.db");
+        }
+    }
+}
